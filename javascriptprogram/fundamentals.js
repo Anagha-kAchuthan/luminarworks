d2639766ac2eab3e32db5bc35bc7749bfd71760c
Anagha <aanagha761@gmail.com>
2021-06-09 22:59:24 +0530

    adding js files

---
+++ javascriptprogram/fundamentals.js
@@ -0,0 +1,14 @@
+// console.log("....Hai...");
+
+
+// var age="23";
+// var p_name="Anagha"
+// console.log("hai iam"+p_name+age+"years old")
+// console.log(typeof(age));
+// console.log(typeof(p_nmae));
+
+var state="kerala";
+var active_cases=15000;
+var cured=20000;
+var death=200;
+console.log(`in ${state} ${active_cases} new cases and ${cured} death ${death} has been reported`);
---
+++ javascriptprogram/swapping.js
@@ -0,0 +1,13 @@
+var num1=10,num2=20;
+console.log(`values before swapping num1=${num1} num2=${num2}`);
+
+// logic for swapping
+// var temp=num1;//num=10
+// var num1=num2;//num1=20
+//  var num2=temp;//num2=10
+
+ var num1=num1+num2;
+ var num2=num1-num2;///num1=10
+ var num1=num1-num2;
+
+console.log(`values after swapping num1=${num1} num2=${num2}`);
