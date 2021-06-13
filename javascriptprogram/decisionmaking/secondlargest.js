68ecc92d22ba1c5eb82bbac846b9df3e56566769
Anagha <aanagha761@gmail.com>
2021-06-13 22:59:40 +0530

    adding loopingfiles

---
+++ javascriptprogram/decisionmaking/secondlargest.js
@@ -0,0 +1,11 @@
+var num1=100;
+var num2=50;
+var num3=30;
+if((num1>num2)&(num1>num3)){
+console.log(`second ${num1}`);
+if(num2>num3){
+}
+else{
+
+}
+}
---
+++ javascriptprogram/ternary.js
@@ -0,0 +1,14 @@
+// largest using ternary
+var num1=10,num2=20;
+var highest=num1>num2?num1:num2;
+console.log(highest);
+
+
+// positive or negative
+var num=10;
+var res=num>0?"+ve":"-ve";
+console.log(res);
+
+var num=10;
+var res=num>0?"+ve":num<0?"negative":"zero";
+console.log(res);
